a992e5e41db3c1bbd4b446e1316781821101e8b8
Anastasias35 <asypacheva35@gmail.com>
2021-06-08 10:41:48 +0300

    fix

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,3 @@
-/*
 package Server;
 
 import Common.Request;
@@ -13,6 +12,9 @@ import java.nio.channels.*;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
@@ -110,54 +112,66 @@ public class Server {
             System.out.println("Запуск сервера");
             System.out.println("Сервер успешно запущен");
             Scanner scanner =new Scanner(System.in);
-            Runnable runnable=()-> {
-                try {
-                    while (true) {
-                        String[] userCommand = (scanner.nextLine().trim() + " ").split(" ", 2);
-                        userCommand[0].trim();
-                        if (userCommand[0].equals("save")) {
-                            Response response = processingOfRequest.getResponse(new Request(userCommand[0], userCommand[1]));
-                        } else if (userCommand[0].equals("exit")) {
-                            System.exit(0);
-                        } else {
-                            System.out.println("Сервер не считывает такую команду с консоли");
-                        }
-                    }
-                }catch ( Exception exception){
-                }
-            };
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                public void run() {
-                    System.out.println("Выход");
-                }
-            });
-            Thread thread=new Thread(runnable);
-            thread.start();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Завершаю программу.");
+            }));
 
 
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.configureBlocking(false);
             ServerSocket serverSocket = serverSocketChannel.socket();
-            serverSocket.bind(new InetSocketAddress(port));
+            serverSocket.bind(new InetSocketAddress("localhost", port));
             selector = Selector.open();
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
             serverSocketChannel.configureBlocking(false);
             while (true) {
+                ExecutorService fixedThreadPool= Executors.newFixedThreadPool(3);
                 connection(serverSocketChannel);
-                Request request = getRequest();
-                System.out.println(request.getArgument());
-                if (request==null) continue;
-                Response response=processingOfRequest.getResponse(request);
-                sendResponse(response);
-                if(response.getResponseType().equals(ResponseType.EXIT)){
-                    System.out.println("Работа с клиентом завершена");
-                    socketChannel.close();
-                }
+                Runnable runnable=new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Request request=getRequest();
+                            Runnable runnable1=new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (request!=null) {
+                                        Response response = processingOfRequest.getResponse(request);
+                                        fixedThreadPool.submit(() -> {
+                                            try {
+                                                sendResponse(response);
+                                                if (response.getResponseType().equals(ResponseType.EXIT)) {
+                                                    System.out.println("Работа с клиентом завершена");
+                                                    socketChannel.close();
+                                                }
+                                            } catch (IOException exception) {
+                                                exception.printStackTrace();
+                                            }
+                                        });
+                                        fixedThreadPool.shutdown();
+                                    }
+                                }
+                            };
+                            Thread thread1=new Thread(runnable1);
+                            thread1.start();
+                            thread1.join();
+                        } catch (IOException exception) {
+                            exception.printStackTrace();
+                        } catch (ClassNotFoundException exception) {
+                            exception.printStackTrace();
+                        } catch (InterruptedException exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                };
+                Thread thread=new Thread(runnable);
+                thread.start();
+                thread.join();
             }
-        }catch (ClassNotFoundException e) {
-            System.out.println("Произошла ошибка при чтении данных");
         }catch (IOException exception){
             System.out.println();
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
         }
     }
 
@@ -172,185 +186,6 @@ public class Server {
         byteArrayOutputStream.close();
         return byteRequest;
     }
-
-    public Request deserialization(byte[] byteRequest) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteRequest);
-        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-        Request request = (Request) objectInputStream.readObject();
-        byteArrayInputStream.close();
-        objectInputStream.close();
-        return request;
-    }
-}
-
-
-
- */
-
-
-
-package Server;
-
-import Common.Request;
-import Common.ResponseType;
-import Server.utilitka.ProcessingOfRequest;
-import Common.Response;
-import Server.utilitka.RequestProcessingThread;
-
-import java.io.*;
-import java.net.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.*;
-import java.util.Scanner;
-import java.util.Set;
-
-public class Server {
-
-    private ServerSocket serverSocket;
-    private int port;
-    private ServerSocketChannel serverSocketChannel;
-    private SocketChannel socketChannel;
-    private Console console;
-    private Selector selector;
-    private SocketAddress socketAddress;
-    ProcessingOfRequest processingOfRequest;
-
-    public Server(int port, ProcessingOfRequest processingOfRequest){
-        this.port=port;
-        this.processingOfRequest=processingOfRequest;
-    }
-
-    public void connection(ServerSocketChannel serverSocketChannel) throws IOException {
-        SocketChannel channel = null;
-        try {
-            while (channel == null) {
-                selector.select();
-                Set<SelectionKey> selectionKeys = selector.selectedKeys();
-                for (SelectionKey selectionKey : selectionKeys) {
-                    if (selectionKey.isAcceptable()) {
-                        channel = serverSocketChannel.accept();
-                        System.out.println("Соединение с клиентом установлено");
-                        selectionKeys.remove(selectionKey);
-                        if (channel != null) {
-                            channel.configureBlocking(false);
-                            channel.register(selector, SelectionKey.OP_READ);
-                        } else break;
-                    }
-                }
-                return;
-            }
-        } catch (BindException exception) {
-            System.out.print("Порт занят,  попробуйте смените значение порта в программе");
-            System.exit(0);
-        }
-    }
-
-    public Request getRequest() throws IOException,ClassNotFoundException {
-            ByteBuffer byteBuffer = ByteBuffer.allocate(65536);
-            Request request = null;
-            while (true) {
-                selector.select();
-                Set<SelectionKey> selectionKeys = selector.selectedKeys();
-                for (SelectionKey selectionKey : selectionKeys) {
-                    if (selectionKey.isReadable()) {
-                        socketChannel = (SocketChannel) selectionKey.channel();
-                        socketChannel.read(byteBuffer);
-                        byteBuffer.flip();
-                        if (byteBuffer.hasRemaining()) {
-                            System.out.println("Чтение запроса...");
-                            request=deserialization(byteBuffer.array());
-                            System.out.println("Обратывается команда " + request.getCommand());
-                        }
-                        socketChannel.configureBlocking(false);
-                        socketChannel.register(selector, SelectionKey.OP_WRITE);
-                        byteBuffer.clear();
-                        selectionKeys.remove(selectionKey);
-                        return request;
-
-                    }
-                }
-            }
-
-    }
-
-
-
-    public void work() {
-        try {
-            System.out.println("Запуск сервера");
-            System.out.println("Сервер успешно запущен");
-            Scanner scanner =new Scanner(System.in);
-            Runnable runnable=()-> {
-                try {
-                    while (true) {
-                        String[] userCommand = (scanner.nextLine().trim() + " ").split(" ", 2);
-                        userCommand[0].trim();
-                        if (userCommand[0].equals("save")) {
-                            Response response = processingOfRequest.getResponse(new Request(userCommand[0], userCommand[1]));
-                        } else if (userCommand[0].equals("exit")) {
-                            System.exit(0);
-                        } else {
-                            System.out.println("Сервер не считывает такую команду с консоли");
-                        }
-                    }
-                }catch ( Exception exception){
-                }
-            };
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                public void run() {
-                    System.out.println("Выход");
-                }
-            });
-            Thread thread=new Thread(runnable);
-            thread.start();
-            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-            serverSocketChannel.configureBlocking(false);
-            ServerSocket serverSocket = serverSocketChannel.socket();
-            InetSocketAddress inetSocketAddress=new InetSocketAddress(port);
-            serverSocket.bind(inetSocketAddress);
-            selector = Selector.open();
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            serverSocketChannel.configureBlocking(false);
-            while (true) {
-                connection(serverSocketChannel);
-                Runnable runnable1=new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Request request = getRequest();
-                            if (request != null) {
-
-                                new RequestProcessingThread(processingOfRequest,request,socketChannel,selector).start();
-
-                                /*Response response = processingOfRequest.getResponse(request);
-                                sendResponse(response);
-                                if (response.getResponseType().equals(ResponseType.EXIT)) {
-                                    System.out.println("Работа с клиентом завершена");
-                                    socketChannel.close();
-                                }
-
-                                 */
-                            }
-                        } catch (IOException exception) {
-                            exception.printStackTrace();
-                        } catch (ClassNotFoundException exception) {
-                            exception.printStackTrace();
-                        }
-                    }
-                };
-                Thread thread1=new Thread(runnable1);
-                thread1.start();
-                try {
-                    thread1.join();
-                } catch (InterruptedException exception) {
-                    exception.printStackTrace();
-                }
-            }
-        } catch (IOException exception){
-            System.out.println();
-        }
-    }
-
 
     public Request deserialization(byte[] byteRequest) throws IOException, ClassNotFoundException {
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteRequest);
